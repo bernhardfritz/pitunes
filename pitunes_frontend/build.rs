1bc6cf1b7c0b09e78386a0625ed0145fd96cfc6c
Bernhard Fritz <bernhard.e.fritz@gmail.com>
2021-09-26 17:42:35 +0200

    trial and error

--- pitunes_frontend/build.rs
+++ pitunes_frontend/build.rs
@@ -2,7 +2,6 @@ use actix_web_static_files::NpmBuild;
 
 fn main() -> std::io::Result<()> {
     NpmBuild::new("./pitunes-frontend")
-        .executable("yarn")
         .install()?
         .run("build")?
         .target("./pitunes-frontend/build")
